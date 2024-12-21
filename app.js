ae57b292dc87916f6a3f9a8678776ee360bdf046
Toqa Khaled <10812022101049@stud.cu.edu.eg>
2024-12-21 04:09:46 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -28,95 +28,55 @@ res.status(400).jason({message: error.message})
 
 
 app.listen(3000,()=>{console.log(" server runing on port 3000")})
+app.post('/signUp',  [body("username","username required and  must have min 4 characterand max 30 character ").isLength({min:4,max:30}).notEmpty(),
+body('password','password required must be min 8 character').isLength({min:8}).notEmpty(),
+body('phoneNumber','wrong phone namber').isLength({min:11,max:11}).notEmpty(),
+body('email','wrong email').isEmail().notEmpty()],async (req, res,next) => {
+
+
+try{
+    //get user object from body
+    let userParam = req.body;
+    // validate
 
 
 
-
-
-app.post('/login',  async (req, res) => {
-
-
-    try{
-        //get user object from body 
-        let userParam = req.body;
-        // validate
-          
-      let user = await User.findOne({email: userParam.email});
-
-
-        if (!user) {
-            res.status(400).send( 'wrong email or password');
-        }
-
-
-const passwordMatch= await bcrypt.compare(userParam.password,user.password)
-        
-        // const salt=await bcrypt.genSalt(10);
-        // req.body.password=await bcrypt.hash(req.body.password,salt)
-    
-       if(passwordMatch){
-        res.status(201).send( ' login successfully!')
-       }else{
-
-        res.status(400).send( 'wrong email or password ')
-       }
-       
-
-    
-    }catch(err)
-    {
-        res.status(400).send('server error: '+ err);
-    }
-
-
-    
-    });
-
-
-
-
-
-    app.put('/update/:email', [body("username","username required and  must have min 4 characterand max 30 character ").isLength({min:4,max:30}).notEmpty(),
-    body('password','password required must be min 8 character').isLength({min:8}).notEmpty(),
-    body('phoneNumber','wrong phone namber').isLength({min:11,max:11}).notEmpty(),
-    body('email','wrong email').isEmail().notEmpty()],async (req, res) => {
-        try{   
-        
-        let userParam = req.body;
-
-          
-     
     const error=validationResult(req);
-      
-    if (!error.isEmpty()) {  
+
+    if (!error.isEmpty()) {
          res.status(400).json( {error : error.array()});
       }
-
-        if(userParam.password){
-    const salt=await bcrypt.genSalt(10);
-    req.body.password=await bcrypt.hash(req.body.password,salt)
-        }
-        
-        let user = await User.findOneAndUpdate({email :req.params.email},{
-            username: userParam.username,
-            email:userParam.email,
-            password:userParam.password,
-            phoneNumber:userParam.phoneNumber
-
-        },{new:true});
+    // const isValid = validator.validate(userParam.email);
 
 
-if(user){
-    res.status(201).json( ' updated successfully!')
-}
+    // if( !isValid){
+    //     res.status(400).json({ message: 'wrong email' });
+    // }
 
-
-
-
-        }catch(err)
-    {
-        res.status(400).json('server error: '+ err);
+    if (await User.findOne({ email: userParam.email })) {
+        res.status(400).send( 'email "' + userParam.email + '" is already exist');
+    }
+    if (await User.findOne({ phoneNumber: userParam.phoneNumber })) {
+        res.status(400).send( 'phone "' + userParam.phoneNumber + '" is already exist');
     }
 
-    });
+
+
+    const salt=await bcrypt.genSalt(10);
+    req.body.password=await bcrypt.hash(req.body.password,salt)
+
+    const user = new User(userParam);
+
+    // save user
+     const result= await user.save();
+
+     res.status(201).send(result)
+
+}catch(err)
+{
+    res.status(400).send('server error: '+ err);
+}
+
+});
+
 
