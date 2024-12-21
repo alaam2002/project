114c1ea1c7d10b5eef6b6ead40271ff511e66696
Toqa Khaled <10812022101049@stud.cu.edu.eg>
2024-12-21 14:09:19 +0200

    edit

--- backend_Users/models/Cart.js
+++ backend_Users/models/Cart.js
@@ -30,3 +30,56 @@ const CartSchema = new Schema(
           );
           const Cartsmodel =mongoose.model("cart", CartSchema);
           module.exports = Cartsmodel;
+
+
+          
+          async function addToCart(userID, productID) {
+            await CartItem.create({
+              userID,
+              productID,
+            });
+          }
+          
+          async function removeFromCart(userID, productID) {
+            await CartItem.findOneAndRemove({
+              userID,
+              productID,
+            });
+          }
+          
+          async function removeProductFromCarts(productID) {
+            await CartItem.deleteMany({
+              productID,
+            });
+          }
+          
+          async function getCartItems(userID) {
+            const cartItems = await CartItem.find({ userID });
+            const productIDs = cartItems.map((ci) => ci.productID);
+            const ids = cartItems.map((ci) => ci.id);
+            let products = [];
+            for (let i = 0; i < productIDs.length; i++) {
+              const product = await Product.findById(productIDs[i]);
+              if (product) {
+                products.push(product);
+              }
+            }
+          
+            for (let i = 0; i < products.length; i++) {
+              products[i] = {
+                _id: products[i].id,
+                name: products[i].name,
+                price: products[i].price,
+                imgUrl: products[i].imgUrl,
+                ciID: ids[i],
+              };
+            }
+          
+            return products;
+          }
+          module.exports = {
+            addToCart,
+            removeFromCart,
+            getCartItems,
+            removeProductFromCarts,
+          };
